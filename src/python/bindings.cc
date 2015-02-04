da8c9abc1a8d92892a23bf30913ca96d24c950ae
Justin Turney <justin.turney@gmail.com>
2015-02-04 11:56:26 -0500

    Added tensor dependency to pytensor.

--- src/python/bindings.cc
+++ src/python/bindings.cc
@@ -1,10 +1,16 @@
 #include <boost/python.hpp>
+#include <boost/python/return_internal_reference.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
 #include <tensor/tensor.h>
 
 using namespace boost::python;
 using namespace tensor;
+
+LabeledTensor tensor_getitem(Tensor& v, const std::string& indx)
+{
+    return v[indx];
+}
 
 BOOST_PYTHON_MODULE (pytensor)
 {
@@ -35,14 +41,42 @@ BOOST_PYTHON_MODULE (pytensor)
     class_<std::vector<double> >("DoubleVector")
             .def(vector_indexing_suite<std::vector<double> >());
 
-    typedef Tensor (Tensor::*build1)(TensorType, const std::string&, const Dimension&);
-    typedef Tensor (Tensor::*build2)(TensorType, const Tensor&);
+    typedef std::vector<double>& (Tensor::*data1)();
+//    typedef Tensor (Tensor::*build1)(TensorType, const std::string&, const Dimension&);
 
     class_<Tensor>("Tensor")
-            .def("build", build1(Tensor::build), "docstring")
+            .def("build", &Tensor::build)
             .staticmethod("build")
-            .def("build", build2(Tensor::build), "docstring")
-            .staticmethod("build")
-            .def("type", &Tensor::type, "docstring")
-            .def("name", &Tensor::name, "docstring");
+            .def("build_from", &Tensor::build_from)
+            .staticmethod("build_from")
+            .add_property("type", &Tensor::type, "docstring")
+            .add_property("name", &Tensor::name, &Tensor::set_name, "docstring")
+            .add_property("dims",
+                          make_function(&Tensor::dims, return_internal_reference<>()), "docstring")
+            .def("dim", &Tensor::dim, "docstring")
+            .add_property("rank", &Tensor::dim, "docstring")
+            .add_property("numel", &Tensor::numel, "docstring")
+//            .def("data", make_function((std::vector<double>& (Tensor::*))&Tensor::data, return_internal_reference<>()), "docstring")
+            .def("data", data1(&Tensor::data), return_internal_reference<>())
+            .def("scale", &Tensor::scale)
+            .def("permute", &Tensor::permute)
+            .def("slice", &Tensor::slice)
+            .def("contract", &Tensor::contract)
+            .def("syev", &Tensor::syev)
+            .def("geev", &Tensor::geev)
+            .def("svd", &Tensor::svd)
+            .def("cholesky", &Tensor::cholesky)
+            .def("lu", &Tensor::lu)
+            .def("qr", &Tensor::qr)
+            .def("cholesky_inverse", &Tensor::cholesky_inverse)
+            .def("inverse", &Tensor::inverse)
+            .def("power", &Tensor::power)
+            .def("print", &Tensor::print)
+            .def("__getitem__", &tensor_getitem);
+
+    class_<LabeledTensor>("LabeledTensor", no_init)
+            .def(self += self)
+            .def(self -= self)
+            .def(self *= double())
+            .def(self /= double());
 }
