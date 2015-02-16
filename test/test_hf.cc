255056985351f6411ebffe6db9a2e269e3c4e043
Justin Turney <justin.turney@gmail.com>
2015-02-16 13:42:45 -0500

    Modifies test/test_hf.cc
    Modifies test case to use tensor::settings::distributed_capable to
    validate against allowed command line options.

--- test/test_hf.cc
+++ test/test_hf.cc
@@ -146,7 +146,7 @@ int main(int argc, char* argv[])
     tensor::initialize(argc, argv);
 
     if (argc > 1) {
-        if (strcmp(argv[1], "cyclops") == 0) {
+        if (settings::distributed_capable && strcmp(argv[1], "cyclops") == 0) {
             tensor_type = kDistributed;
             printf("  *** Testing distributed tensors. ***\n");
         }
