36d1db26d6bf8c8612cfa6806b2a6dea0e062d3e
Justin Turney <justin.turney@gmail.com>
2016-01-04 10:02:03 -0500

    Forgot to add integrals

---
+++ include/ambit/helpers/psi4/integrals.h
@@ -0,0 +1,28 @@
+//
+// Created by Justin Turney on 12/17/15.
+//
+
+#ifndef AMBIT_INTEGRALS_H
+#define AMBIT_INTEGRALS_H
+
+#include <libmints/mints.h>
+
+namespace ambit {
+
+class Tensor;
+
+namespace helpers {
+
+namespace psi4 {
+
+void integrals(psi::OneBodyAOInt* integral, ambit::Tensor* target);
+
+void integrals(psi::TwoBodyAOInt* integral, ambit::Tensor* target);
+
+} // namespace psi4
+
+} // namespace helpers
+
+} // namespace ambit
+
+#endif //AMBIT_INTEGRALS_H
