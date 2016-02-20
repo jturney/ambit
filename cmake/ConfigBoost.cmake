ae907b99612a3c0cc0a11cce407cfe64f1dcfd31
Justin Turney <justin.turney@gmail.com>
2016-02-20 15:37:17 -0500

    More dependency testing

--- cmake/ConfigBoost.cmake
+++ cmake/ConfigBoost.cmake
@@ -1,7 +1,7 @@
 # Oldest version of Boost tested against
 set(BOOSTVER 1.53.0)
 
-list(APPEND needed_components)
+list(APPEND needed_components filesystem system)
 
 if(ENABLE_PYTHON)
     # On Macs with Boost Python installed with Homebrew with Python 3 the library
