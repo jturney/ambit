7ee4e396b582760a3b68bc7486f2efb03bffe56c
Kevin Hannon <kannon1992@gmail.com>
2016-11-04 15:47:51 -0400

    Made a few changes to avoid linking with boost

--- src/io/psi4/manager.cc
+++ src/io/psi4/manager.cc
@@ -16,10 +16,10 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-151 USA.
  */
 
-#include <boost/filesystem.hpp>
-#include <boost/lexical_cast.hpp>
+//#include <boost/filesystem.hpp>
+//#include <boost/lexical_cast.hpp>
 
-#include <ambit/io/psi4/io.h>
+//#include <ambit/io/psi4/io.h>
 
 //#include <util/print.h>
 
@@ -27,63 +27,63 @@
 #include <mpi.h>
 #endif
 
-namespace ambit
-{
-namespace io
-{
-namespace psi4 {
+//namespace ambit
+//{
+//namespace io
+//{
+//namespace psi4 {
+//
+//namespace {
 
-namespace {
-
-void create_directory(const std::string& directory)
-{
-    if (boost::filesystem::exists(directory) == false) {
-        // create directory
-        boost::filesystem::create_directory(directory);
-    }
-    else if (boost::filesystem::is_directory(directory) == false) {
-        // it exists and is not a directory
-        throw std::runtime_error("base directory name given already exists and "
-                                         "is not a directory: " +
-                                 directory);
-    }
-}
-}
-
-Manager::Manager(const std::string& base_directory)
-        : base_directory_(base_directory)
-{
-    create_directory(base_directory_);
-
-// if we are running with MPI create a "local" scratch directory
-#if defined(HAVE_MPI)
-    int flag = 0;
-    MPI_Initialized(&flag);
-    if (flag)
-    {
-        mpi_base_directory_ =
-            base_directory_ + "/" +
-            boost::lexical_cast<std::string>(MPI::COMM_WORLD.Get_rank());
-        create_directory(mpi_base_directory_);
-    }
-    else
-        mpi_base_directory_ = base_directory_;
-#else
-    mpi_base_directory_ = base_directory_;
-#endif
-
-    // append tailing '/' to the directories to make our lives easier
-    base_directory_ += "/";
-    mpi_base_directory_ += "/";
-
-    printf("base_directory_ %s\n", base_directory_.c_str());
-    printf("mpi_base_directory_ %s\n", mpi_base_directory_.c_str());
-}
-
-File Manager::scratch_file(const std::string& basename)
-{
-    return File(mpi_base_directory_ + basename, kOpenModeOpenExisting);
-}
-}
-}
-}
+//void create_directory(const std::string& directory)
+//{
+//    if (boost::filesystem::exists(directory) == false) {
+//        // create directory
+//        boost::filesystem::create_directory(directory);
+//    }
+//    else if (boost::filesystem::is_directory(directory) == false) {
+//        // it exists and is not a directory
+//        throw std::runtime_error("base directory name given already exists and "
+//                                         "is not a directory: " +
+//                                 directory);
+//    }
+//}
+//}
+//
+//Manager::Manager(const std::string& base_directory)
+//        : base_directory_(base_directory)
+//{
+//    create_directory(base_directory_);
+//
+//// if we are running with MPI create a "local" scratch directory
+//#if defined(HAVE_MPI)
+//    int flag = 0;
+//    MPI_Initialized(&flag);
+//    if (flag)
+//    {
+//        mpi_base_directory_ =
+//            base_directory_ + "/" +
+//            boost::lexical_cast<std::string>(MPI::COMM_WORLD.Get_rank());
+//        create_directory(mpi_base_directory_);
+//    }
+//    else
+//        mpi_base_directory_ = base_directory_;
+//#else
+//    mpi_base_directory_ = base_directory_;
+//#endif
+//
+//    // append tailing '/' to the directories to make our lives easier
+//    base_directory_ += "/";
+//    mpi_base_directory_ += "/";
+//
+//    printf("base_directory_ %s\n", base_directory_.c_str());
+//    printf("mpi_base_directory_ %s\n", mpi_base_directory_.c_str());
+//}
+//
+//File Manager::scratch_file(const std::string& basename)
+//{
+//    return File(mpi_base_directory_ + basename, kOpenModeOpenExisting);
+//}
+//}
+//}
+//}
