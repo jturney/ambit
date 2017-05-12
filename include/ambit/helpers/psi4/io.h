7d30c6374c740eae1aedf05eba87857fad2483c3
Justin Turney <justin.turney@gmail.com>
2017-05-12 13:07:01 -0400

    Add LGPL-3.0 license

--- include/ambit/helpers/psi4/io.h
+++ include/ambit/helpers/psi4/io.h
@@ -1,3 +1,32 @@
+/*
+ * @BEGIN LICENSE
+ *
+ * ambit: C++ library for the implementation of tensor product calculations
+ *        through a clean, concise user interface.
+ *
+ * Copyright (c) 2014-2017 Ambit developers.
+ *
+ * The copyrights for code used from other parties are included in
+ * the corresponding files.
+ *
+ * This file is part of ambit.
+ *
+ * Ambit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Ambit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with ambit; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @END LICENSE
+ */
+
 #if !defined(TENSOR_HELPERS_PSI4_IO_H)
 #define TENSOR_HELPERS_PSI4_IO_H
 
--- include/ambit/io/psi4/file.h
+++ include/ambit/io/psi4/file.h
@@ -1,19 +1,30 @@
 /*
- *  Copyright (C) 2013  Justin Turney
+ * @BEGIN LICENSE
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
-
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
-
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-151 USA.
+ * ambit: C++ library for the implementation of tensor product calculations
+ *        through a clean, concise user interface.
+ *
+ * Copyright (c) 2014-2017 Ambit developers.
+ *
+ * The copyrights for code used from other parties are included in
+ * the corresponding files.
+ *
+ * This file is part of ambit.
+ *
+ * Ambit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Ambit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with ambit; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @END LICENSE
  */
 
 #if !defined(TENSOR_IO_FILE)
